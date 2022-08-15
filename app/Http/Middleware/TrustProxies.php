7320b1294b7a5326c633103c5329b81dfd57a67a
Adam Miedema <armgitaar@yahoo.com>
2022-08-15 17:47:54 -0600

    set trusted proxies

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,9 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies;
+    protected $proxies = [
+        '149.102.150.139'
+    ];
 
     /**
      * The headers that should be used to detect proxies.
